6642a49313503fffb896eceeb459fd3a836e0f3b
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-10-28 16:55:18 +0700

    commit

---
+++ HTML_CSS_01_COMPILATION/QUAN_LY_THANH_PHAM/assets/js/scripts.js
@@ -0,0 +1,19 @@
+/* =============================================================================================================
+=======================================...Tái sử dụng code
+================================================================================================================ */
+
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        document.querySelector(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                document.querySelector(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        });
+}
