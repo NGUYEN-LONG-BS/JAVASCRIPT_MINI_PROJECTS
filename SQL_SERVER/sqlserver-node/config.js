8f9b93804596d46c797be1c767e89aa4eccb6470
NGUYEN-LONG-BS <nguyenlongbs2012@gmail.com>
2024-10-30 16:04:20 +0700

    commit

---
+++ SQL_SERVER/sqlserver-node/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    user: 'your_username',
+    password: 'your_password',
+    server: 'your_server',
+    database: 'your_database',
+    options: {
+    // encrypt: true, // Sử dụng nếu bạn kết nối với Azure
+    // trustServerCertificate: true // Sử dụng nếu bạn kết nối với máy chủ cục bộ
+    }
+    };
